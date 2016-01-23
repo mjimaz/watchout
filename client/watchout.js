cdeebca9f0fc135b9f5139f032a429e2a8c2f689
mjimaz <m.aljimaz@gmail.com>
2016-01-22 16:23:21 -0800

    Implement enemies moving around randomly

--- client/watchout.js
+++ client/watchout.js
@@ -1,16 +1,42 @@
 // start slingin' some d3 here.
+var generateRandomPosition = function() {
+   var randomPosition = [];
+  for ( var i = 0 ; i < 10; i++ ) {
+    var randomX = Math.floor(Math.random() * (width - 30));
+    var randomY = Math.floor(Math.random() * (height - 30));
+    randomPosition.push([randomX, randomY]);
+  }
+  return randomPosition;
+};
+var movingEnemies = function() {
+  var randomPosition = generateRandomPosition();
+  d3.selectAll('.enemy').data(randomPosition)
+                      .transition()
+                      .duration(1500)
+                      .style("cx", function( d ){ return d[0] + 'px'; } )
+                      .style('cy', function( d ){ return d[1] + 'px'; } );
+
+};
+
 var width = 500;
 var height = 500;
 var svg = d3.select('body').append('svg').style("width", width).style("height", height);
-var randomPosition = [];
-for ( var i = 0 ; i < 10; i++ ) {
-  var randomX = Math.floor(Math.random() * (width - 30));
-  var randomY = Math.floor(Math.random() * (height - 30));
-  randomPosition.push([randomX, randomY]);
-}
+var randomPosition = generateRandomPosition();
 svg.selectAll('circle').data(randomPosition)
                  .enter().append('circle')
                  .attr('class', 'enemy')
                  .style("cx", function( d ){ return d[0] + 'px'; } )
                  .style('cy', function( d ){ return d[1] + 'px'; } );
                  //.attr('xlink:href', 'asteroid.png');
+setInterval(movingEnemies, 1000);
+
+
+
+
+
+
+
+
+
+
+
