c4baf9c9b1a7bb6656845704cb06c92eb04c0fce
mjimaz <m.aljimaz@gmail.com>
2016-01-22 16:04:54 -0800

    Implement svg circle and random position

--- client/watchout.js
+++ client/watchout.js
@@ -1,4 +1,16 @@
 // start slingin' some d3 here.
-var svg = d3.select('body').append('svg');
-svg.style("width", 500);
-svg.style("height", 500);
+var width = 500;
+var height = 500;
+var svg = d3.select('body').append('svg').style("width", width).style("height", height);
+var randomPosition = [];
+for ( var i = 0 ; i < 10; i++ ) {
+  var randomX = Math.floor(Math.random() * (width - 30));
+  var randomY = Math.floor(Math.random() * (height - 30));
+  randomPosition.push([randomX, randomY]);
+}
+svg.selectAll('circle').data(randomPosition)
+                 .enter().append('circle')
+                 .attr('class', 'enemy')
+                 .style("cx", function( d ){ return d[0] + 'px'; } )
+                 .style('cy', function( d ){ return d[1] + 'px'; } );
+                 //.attr('xlink:href', 'asteroid.png');
